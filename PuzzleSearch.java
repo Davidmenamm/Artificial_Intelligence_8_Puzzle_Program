f5434b2b2794546114fd7c2c4263865c347b952d
asar1245 <sebasar1245@gmail.com>
2020-10-15 12:50:52 -0500

    feat: A* code with Manhatan heuristic

---
+++ PuzzleSearch.java
@@ -0,0 +1,57 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class PuzzleSearch {
+
+	public static void main(String[] args) {
+		PuzzleSearch program;
+		try {
+			program = new PuzzleSearch();
+			program.run();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void run() {
+		Scanner sc = new Scanner(System.in);
+		int choice = 0;
+		System.out.println("------------------");
+		System.out.println("8 Puzzle Search");
+		System.out.println("------------------");
+		System.out.println("[1] A* Search without heuristic\n"
+				+ "[2] A* Search with Manhattan Distance heuristic\n"
+				+ "[3] A* Search with learned heuristic\n");
+		try {
+			System.out.print("Enter Choice: ");
+			choice = Integer.parseInt(sc.nextLine());
+		} catch (Exception e) {
+			System.out.println("Please enter a valid choice.");
+		}
+
+		sc.close();
+		try {
+			determineSearchAlgo(choice);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void determineSearchAlgo(int c) throws IOException {
+		switch (c) {
+		case 1:
+			new AStarSearch().run(false);
+			break;
+		case 2:
+			new AStarSearch().run(true);
+			break;
+		case 3:
+			//Pending
+
+			break;
+		default:
+			System.out.println("Please enter one of the choices (1-5).");
+		}
+	}
+
+}
---
+++ out/production/eight-puzzle-search-develop/README.md
@@ -0,0 +1,26 @@
+# Eight Puzzle
+A java program that solves the Eight Puzzle problem using five different search algorithms.
+
+## Problem
+The 8 puzzle consists of eight numbered, movable tiles set in a 3x3 frame. One cell of the frame is always empty thus making it possible to move an adjacent numbered tile into the empty cell. The following shows a sequence of legal moves from an initial board to the goal board.
+
+![image](https://user-images.githubusercontent.com/25243082/72701408-30e41100-3b8a-11ea-9d22-0ee4c264bf15.png)
+
+The goal is to change the initial configuration into the goal configuration.
+
+## System Design
+
+### Pipeline Tasks
+- [ ] Add Blind Search
+- [ ] Fix code structure
+- [ ] Add in package
+- [ ] Code cleanup
+
+## Resources
+[8 Puzzle Java](https://www.cs.princeton.edu/courses/archive/fall15/cos226/assignments/8puzzle.html)\
+[8 Puzzle A* Search](https://blog.goodaudience.com/solving-8-puzzle-using-a-algorithm-7b509c331288)\
+[8 Puzzle](https://www.d.umn.edu/~jrichar4/8puz.html)
+
+## Support
+For any questions or concerns, please email [shaniaddev@gmail.com](mailto:shaniaddev@gmail.com?subject=[GitHub]%20Eight%20Puzzle%20Search)
+
